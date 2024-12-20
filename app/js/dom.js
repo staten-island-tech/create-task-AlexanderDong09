4b9518244cecadfb4de57aa97d0ffa28211d99cf
AlexanderDong09 <alexanderdong09@gmail.com>
2024-12-19 22:21:51 -0500

    some function progress

--- app/js/dom.js
+++ app/js/dom.js
@@ -2,6 +2,8 @@ const DOMSelectors = {
   container: document.querySelector(".container"),
   side1: document.querySelector("#side1"),
   side2: document.querySelector("#side2"),
+  moreRecent: document.querySelector("#more-recent"),
+  lessRecent: document.querySelector("#less-rcent"),
 };
 
 export { DOMSelectors };
--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,8 @@
 import { DOMSelectors } from "./dom.js";
 
+let photoPool = [];
+
 async function fetchImages(count = 5) {
-  // 2nd api call, getting the data from a specific date
   try {
     const response = await fetch(
       `https://api.nasa.gov/planetary/apod?api_key=ul8UJtZB9tVcNUR2v9fwokows0p7JuQ4atB6G65d&count=${count}` // fetch returns a promise (a promise that you'll get something) (like a receipt)
@@ -17,4 +18,32 @@ async function fetchImages(count = 5) {
     console.log(error);
     console.log("sorry coudlnt fid that");
   }
+  photoPool.push(...data); // pushes api data into array. use w/ shift and maybe to display card info by using an index (at the end?)
 }
+
+function startGame() {
+  const leftPhoto = photoPool.shift();
+  const rightPhoto = photoPool.shift();
+
+  displayPhotos(leftPhoto, rightPhoto);
+}
+
+function displayPhotos(leftPhoto, rightPhoto) {
+  document.querySelector("#side1").innerHTML = `
+    <h2>${leftPhoto.title}</h2>
+    <img src="${item.hdurl}" alt="${item.title}
+    <p>Was APOD on: ${leftPhoto.date}</p>
+  `;
+
+  document.querySelector("#side2").innerHTML = `
+    <h2>${rightPhoto.title}</h2>
+    <img src="${item.hdurl}" alt="${item.title}
+    <button class="btn" id="more-recent">More Recent</button>
+    <button class="btn" id="less-recent">Less Recent</button>
+    <p>Featured on: ???</p>
+  `;
+
+  // make sure to set up the buttons function and put here
+}
+
+startGame();
