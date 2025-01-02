fe9cbd005f64edda0c8653c83136612612f67aa0
AlexanderDong09 <alexanderdong09@gmail.com>
2025-01-02 18:13:40 -0500

    u

--- app/js/main.js
+++ app/js/main.js
@@ -2,19 +2,21 @@ import { DOMSelectors } from "./dom.js";
 
 let photoPool = [];
 
+// This code was made freely available by the NASA APOD (Astronomy Photo of the Day) API.
 async function fetchImages(count = 5) {
   try {
     const response = await fetch(
-      `https://api.nasa.gov/planetary/apod?api_key=ul8UJtZB9tVcNUR2v9fwokows0p7JuQ4atB6G65d&count=${count}` // fetch returns a promise (a promise that you'll get something) (like a receipt)
+      `https://api.nasa.gov/planetary/apod?api_key=ul8UJtZB9tVcNUR2v9fwokows0p7JuQ4atB6G65d&count=${count}`
     );
-    // gaurd clause
     if (response.status != 200) {
       throw new Error(response);
     } else {
       const data = await response.json();
       console.log(data);
-      photoPool.push(...data); // pushes api data into array. use w/ shift and maybe to display card info by using an index (at the end?)
-      return data;
+
+      const filteredData = data.filter((item) => item.media_type != "video");
+      photoPool.push(...filteredData); // pushes api data into array. use w/ shift and maybe to display card info by using an index (at the end?)
+      return filteredData;
     }
   } catch (error) {
     console.log(error);
@@ -22,7 +24,8 @@ async function fetchImages(count = 5) {
   }
 }
 
-function startGame() {
+async function startGame() {
+  await fetchImages();
   const leftPhoto = photoPool.shift();
   const rightPhoto = photoPool.shift();
 
@@ -30,21 +33,38 @@ function startGame() {
 }
 
 function displayPhotos(leftPhoto, rightPhoto) {
-  DOMSelectors.container.querySelector("#side1").innerHTML = `
-    <h2>${leftPhoto.title}</h2>
-    <img src="${item.hdurl}" alt="${item.title}
-    <p>Was APOD on: ${leftPhoto.date}</p>
-  `;
+  DOMSelectors.side1.innerHTML = "";
+  DOMSelectors.side2.innerHTML = "";
 
-  DOMSelectors.container.querySelector("#side2").innerHTML = `
-    <h2>${rightPhoto.title}</h2>
-    <img src="${item.hdurl}" alt="${item.title}
-    <button class="btn" id="more-recent">More Recent</button>
-    <button class="btn" id="less-recent">Less Recent</button>
-    <p>Featured on: ???</p>
-  `;
-
+  DOMSelectors.side1.insertAdjacentHTML(
+    "beforeend",
+    `
+  <h2>${leftPhoto.title}</h2>
+  <img src="${leftPhoto.hdurl}" alt="${leftPhoto.title}">
+  <h4>Was APOD on:</h4>
+  <h2>${leftPhoto.date}</h2>`
+  );
+  DOMSelectors.side2.insertAdjacentHTML(
+    "beforeend",
+    `<h2 class="text-3xl">${rightPhoto.title}</h2>
+    <img src="${rightPhoto.hdurl}" alt="${rightPhoto.title}">
+    <p>Was featured</p>
+    <button id="more-recent" class="btn btn-neutral box-border drop-shadow-sm w-20 bg-violet-700 text-white p-2 m-2 rounded">More Recently!</button>
+    <button id="less-recent" class="btn btn-neutral box-border drop-shadow-2xl w-20 bg-fuchsia-800 text-white p-2 m-2 rounded">Less Recently!</button>
+    <h4 class="text-xl">than ${leftPhoto.title}</h4>`
+  );
   // make sure to set up the buttons function and put here
+}
+
+function guessing(leftPhoto, rightPhoto, choice) {
+  const leftImageDate = new Date(leftPhoto.date);
+  const rightImageDate = new Date(rightPhoto.date);
+
+  const correct =
+    (choice === "More Recent" && rightImageDate > leftImageDate) ||
+    (choice === "Less Recent" && leftImageDate > rightImageDate);
+
+  while (correct) {}
 }
 
 startGame();
